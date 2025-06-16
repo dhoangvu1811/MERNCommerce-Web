5eecaaa2464b73c0a2cc8429c8874cebda30c779
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-06-16 17:05:18 +0700

    add cssBaseLine & dark light mode

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -13,21 +14,58 @@ export default [
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+        sourceType: 'module'
+      }
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
+      ...react.configs.recommended.rules,
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 0,
+      'react/display-name': 0,
+      'react/react-in-jsx-scope': 'off',
+
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: ['@mui/*/*/*']
+        }
+      ],
+
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      indent: ['warn', 2],
+      semi: [1, 'never'],
+      quotes: ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
+    }
+  }
 ]
---
+++ src/theme.js
@@ -0,0 +1,25 @@
+import { createTheme } from '@mui/material/styles'
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+
+// Create a theme instance.
+const theme = createTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  },
+  cssVariables: {
+    colorSchemeSelector: 'class'
+  }
+})
+
+export default theme
